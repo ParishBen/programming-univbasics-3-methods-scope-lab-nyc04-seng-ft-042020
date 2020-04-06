86011d6b41ecdbf9c8e1a8dc02dfc910c7cddae6
Ben Parish <parishben13@gmail.com>
2020-04-06 01:10:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,12 +13,6 @@ def link
 end
 
 def all_phrases(one)
-case one
-  when toadstool
-    puts "Thank You Mario! But Our Princess Is In Another Castle!"
-   when link
-  puts "It's Dangerous To Go Alone! Take This."
-when mario
   puts "It's-a me, Mario!"
 end
 
